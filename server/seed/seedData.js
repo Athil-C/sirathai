93a29d290dabd0368509e5e7c73a1c378753aa2a
Athil c <athilchalil@gmail.com>
2026-07-11 06:34:22 +0530

    third

--- server/seed/seedData.js
+++ server/seed/seedData.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from root first, then server folder
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, '../.env') });
 dotenv.config();
 
 import User from '../models/User.js';
